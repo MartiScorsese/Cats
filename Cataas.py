061844b3e7e1e8cddcfbe158ab2b24b00c336ebd
MartiScorsese <9456664@mail.ru>
2025-07-29 14:18:26 +0300

    Добавлено меню вместо кнопки обновления

--- Cataas.py
+++ Cataas.py
@@ -26,6 +26,10 @@ def set_image():
         label.config(image=img)
         label.image = img
 
+ def exit():
+     window.destroy()
+
+
 
 window = Tk()
 window.title('Cats!')
@@ -34,8 +38,17 @@ window.geometry('600x520')
 label = Label(window, text='Cats!')
 label.pack()
 
-update_button = Button(text='Обновить', command=set_image)
-update_button.pack()
+# update_button = Button(text='Обновить', command=set_image)
+# update_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(menu=file_menu, label='File')
+file_menu.add_command(label='Новое фото', command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command=exit)
 
 url = 'https://cataas.com/cat'
 
